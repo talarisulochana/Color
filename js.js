512d24e40e1acacb63182d58ae7037fa1bf297ed
talarisulochana <talarisulochana123@gmail.com>
2024-11-17 18:23:24 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,39 @@
+ let ColorChange=document.getElementById("Color")
+
+ function a(){
+    document.body.style.backgroundColor="lawngreen";
+
+ }
+
+ function b(){
+    document.body.style.backgroundColor="aqua";
+ }
+
+ function c(){
+    document.body.style.backgroundColor="lemonchiffon";
+ }
+
+ function d(){
+    document.body.style.backgroundColor="lightcoral";
+ }
+
+ function e(){
+    document.body.style.backgroundColor="lightskyblue";
+ }
+ function f(){
+    document.body.style.backgroundColor=" violet";
+ }
+
+ function g(){
+    document.body.style.backgroundColor=" lightpink";
+ }
+ function h(){
+    document.body.style.backgroundColor="lightseagreen";
+ }
+ function i(){
+    document.body.style.backgroundColor="brown";
+
+ }
+ function j(){
+    document.body.style.backgroundColor="blueviolet";
+ }
